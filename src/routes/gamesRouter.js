2fa3eaa420cd8b44b2c6381c230cbee6e77c6126
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-05-27 09:48:10 -0300

    implements create game and refact code in get games

--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getGames } from "../controllers/gameController.js";
+import { createGame, getGames } from "../controllers/gameController.js";
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
+gamesRouter.post("/games", createGame);
 
 export default gamesRouter;
---
+++ src/schemas/gameSchemas.js
@@ -0,0 +1,9 @@
+import joi from 'joi';
+
+export const gameSchema = joi.object({
+    name: joi.string().required(),
+    image: joi.string().required(),
+    stockTotal: joi.number().min(1).required(),
+    pricePerDay: joi.number().min(1).required(),
+    categoryId: joi.number().required(),
+});
