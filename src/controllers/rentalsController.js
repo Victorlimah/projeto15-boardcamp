a39998f6b91dd3dc3f0ec0fbb684f1687d91e195
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-05-29 22:30:35 -0300

    implements finallate rental route

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -85,6 +85,35 @@ export async function createRental(req, res){
   }
 }
 
+export async function finallateRental(req, res){
+  try{
+    const { id } = req.params;
+
+    const rental = await connection.query(`SELECT * FROM rentals WHERE id=$1`, [id]);
+    if(rental.rowCount === 0)
+      return res.status(404).send({ message: "Rental not found"});
+
+    const { returnDate, rentDate, daysRented, originalPrice } = rental.rows[0];
+
+    if(returnDate)
+      return res.status(400).send({ message: "Rental already returned"});
+
+    const date = new Date();
+    const daysFinal = date.getDate() - rentDate.getDate();
+    let delayFee = 0;
+
+    if(daysFinal > daysRented)
+      delayFee = (daysFinal - daysRented) * originalPrice;
+    
+    await connection.query(`
+      UPDATE rentals SET "returnDate"=$1, "delayFee"=$2  WHERE id=$3`, [date, delayFee, id]);
+
+   res.status(200).send({ message: "Rental finalized successfully" });
+  } catch (err){
+    res.status(500).send({ message: "Error finalizing rental", error: err });
+  }
+}
+
 export async function deleteRental(req, res){
   const { id } = req.params;
 
@@ -122,7 +151,7 @@ function rentalsFactory(row) {
     id,
     customerId,
     gameId,
-    rentDate: rentDate.toISOString().slice(0, 10),
+    rentDate,
     daysRented,
     returnDate,
     originalPrice,
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createRental, deleteRental, getRentals } from "../controllers/rentalsController.js";
+import { createRental, deleteRental, finallateRental, getRentals } from "../controllers/rentalsController.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
 rentalsRouter.post("/rentals", createRental);
+rentalsRouter.post("/rentals/:id/return", finallateRental );
 rentalsRouter.delete("/rentals/:id", deleteRental);
 
 export default rentalsRouter;
